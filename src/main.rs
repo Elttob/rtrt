bf14c98acb1466dafd5ead398e451fba29bede8a
Elttob <dan.bottlefact@gmail.com>
2023-05-26 15:49:05 +0100

    Goodbye ash

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,9 @@
 use std::sync::Arc;
 
 use anyhow::Result;
-use glam::vec2;
-use winit::{window::{WindowBuilder, Window}, event_loop::{EventLoop, ControlFlow}, event::{Event, WindowEvent}};
+use winit::{window::{WindowBuilder, Window}, event_loop::EventLoop, event::};
 
-use crate::{render::Renderer, input::Input};
-
-mod vulkan;
-mod input;
-mod render;
-
-fn create_window() -> Result<(EventLoop<()>, Arc<Window>)> {
+fn create_window() -> Result<(EventLoop<()>, Window)> {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
         .with_title("Real Time Ray Tracing")
@@ -29,7 +22,7 @@ fn create_window() -> Result<(EventLoop<()>, Arc<Window>)> {
         (monitor_size.height - window_size.height) / 2
     ));
 
-    Ok((event_loop, Arc::new(window)))
+    Ok((event_loop, window))
 }
 
 fn main() -> Result<()> {
@@ -39,44 +32,5 @@ fn main() -> Result<()> {
     log::debug!("Starting!");
 
     let (event_loop, window) = create_window()?;
-    let mut renderer = Renderer::new(window.clone())?;
     
-    // TODO: 1.3.3.1 here -> https://github.com/adrien-ben/vulkan-tutorial-rs/commits/master?after=6c47737e505aa7b2b5a4d7b2711490b2482c246b+34&branch=master&qualified_name=refs%2Fheads%2Fmaster
-
-    {
-        let mut cursor_over_window = false;
-        let mut input = Input::new(vec2(0.0, 0.0));
-
-        window.set_visible(true);
-        event_loop.run(move |event, _, control_flow| match event {
-            Event::WindowEvent { ref event, window_id } if window_id == window.id() => match event {
-                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                WindowEvent::Resized(_) => renderer.resize(),
-                WindowEvent::ScaleFactorChanged { .. } => renderer.resize(),
-                WindowEvent::CursorEntered { .. } => cursor_over_window = true,
-                WindowEvent::CursorLeft { .. } => cursor_over_window = true,
-                _ => if window.has_focus() && cursor_over_window {
-                    input.process_window_events(event);
-                }
-            },
-            Event::DeviceEvent { ref event, .. } => if window.has_focus() && cursor_over_window {
-                input.process_device_events(event);
-                let window_size = window.inner_size();
-                window.set_cursor_position(winit::dpi::PhysicalPosition::new(
-                    window_size.width / 2,
-                    window_size.height / 2
-                )).expect("Platform does not support setting the cursor position");
-            },
-            Event::MainEventsCleared => {
-                // let snapshot = input.snapshot();
-                // scene.camera.pitch_yaw_radians = snapshot.pitch_yaw_radians;
-                // let move_speed = 2.0 / 144.0;
-                // scene.camera.position += snapshot.move_axes.z * move_speed * pitch_yaw::look_dir(scene.camera.pitch_yaw_radians);
-                // scene.camera.position += snapshot.move_axes.y * move_speed * Vec3::Y;
-                // scene.camera.position += snapshot.move_axes.x * move_speed * pitch_yaw::flat_right_vec(scene.camera.pitch_yaw_radians.y);
-                let _did_render_frame = renderer.render().unwrap();
-            }
-            _ => (),
-        });
-    }
 }
